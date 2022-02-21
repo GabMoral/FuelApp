427f70d96b1f37b72d2ceda783acb4e1cde7d32b
GabMoral <100104823+GabMoral@users.noreply.github.com>
2022-02-20 22:35:23 -0600

    Added UI
    Files with the added textboxes.

--- frontend/src/components/Fuel/quote.js
+++ frontend/src/components/Fuel/quote.js
@@ -1,9 +1,46 @@
 import React, {Component} from 'react';
 
 export default class Quote extends Component {
+    handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            username: this.username,
+            password: this.password,
+            deliveryAddress: "123 Main St."
+        };
+        console.log(data);
+        const adminLogin = {username: "admin1", password: "admin123"}
+        if (this.username == adminLogin.username && this.password == adminLogin.password) {
+            console.log("Logged in");
+        }
+    };
     render() {
         return (
-            <h2>Fuel Quote Page</h2>
+            
+            <form onSubmit={this.handleSubmit}>
+                <h3>Fuel Quote</h3>
+
+                <div className="form-group">
+                    <label>Gallons requested</label>
+                    <input type="number" className="form-control" placeholder="Whole Gallons" 
+                        onChange={e => this.gallons = e.target.value}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Delivery Address</label>
+                    <input type="text" className="form-control" value="123 Main St." readonly />
+                </div>
+                <div className="form-group">
+                    <label>Delivery Date</label>
+                    <input type="date" className="form-control" placeholder="Delivery Date" 
+                        onChange={e => this.deliveryDate = e.target.value}/>
+                </div>
+                <div className="form-group">
+                    <label>Suggested price/gallon</label>
+                    <input type="text" className="form-control" value="$0.00" readonly />
+                </div>
+                <button className="btn btn-primary btn-block">Submit</button>
+            </form>
         )
     }
 }
