df3ed1142df0694bf939e29c545ac9f8b8df0c40
Kyara <zkyara12@gmail.com>
2022-04-26 04:13:16 -0500

    changed profile

---
+++ frontend/src/components/Profile/oldProfileSave.js
@@ -0,0 +1,116 @@
+import React, {Component} from 'react';
+import ReactDOM from "react-dom";
+
+export default class Profile extends Component {
+    handleSubmit = e => {
+        
+        e.preventDefault();
+        const data = {
+            name: this.name,
+            address1: this.address1,
+            address2: this.address2,
+            city: this.city,
+            state: this.state,
+            zipcode: this.zipcode
+        };
+        
+        console.log(data);
+        
+    };
+
+    render() {
+        return (
+            <form>
+                <h3>Profile</h3>
+
+                <div className="form-group">
+                    <label>Full name</label>
+                    <input type="text" maxlength="50" className="form-control" placeholder="Full name" 
+                        onChange={e => this.name = e.target.value}/>
+                </div>
+                <br></br>
+                <div className="form-group">
+                    <label>Address 1</label>
+                    <input type="text" maxlength="100" className="form-control" placeholder="Address 1" 
+                        onChange={e => this.address1 = e.target.value}/>
+                </div>
+                <br></br>
+                <div className="form-group">
+                    <label>Address 2</label>
+                    <input type="text" maxlength="100" className="form-control" placeholder="Address 2" 
+                        onChange={e => this.address2 = e.target.value}/>
+                </div>
+                <br></br>
+                <div className="form-group">
+                    <label>City</label>
+                    <input type="text" maxlength="100" className="form-control" placeholder="City" 
+                        onChange={e => this.city = e.target.value}/>
+                </div>
+                <br></br>
+                <div className="form-group">
+                    <label>State</label>
+                    <select onChange={e => this.state = e.target.value}>
+                        <option value="Alabama">AL</option>
+                        <option value="Alaska">AK</option>
+                        <option value="Arizona">AZ</option>
+                        <option value="Arkansas">AR</option>
+                        <option value="California">CA</option>
+                        <option value="Colorado">CO</option>
+                        <option value="Conneticut">CT</option>
+                        <option value="Delaware">DE</option>
+                        <option value="Florida">FL</option>
+                        <option value="Georgia">GA</option>
+                        <option value="Hawaii">HI</option>
+                        <option value="Idaho">ID</option>
+                        <option value="Illinois">IL</option>
+                        <option value="Indiana">IN</option>
+                        <option value="Iowa">IA</option>
+                        <option value="Kansas">KS</option>
+                        <option value="Kentucky">KY</option>
+                        <option value="Louisiana">LA</option>
+                        <option value="Maine">ME</option>
+                        <option value="Maryland">MD</option>
+                        <option value="Massachusetts">MA</option>
+                        <option value="Michigan">MI</option>
+                        <option value="Minnesota">MN</option>
+                        <option value="Mississippi">MS</option>
+                        <option value="Missouri">MO</option>
+                        <option value="Montana">MT</option>
+                        <option value="Nebraska">NE</option>
+                        <option value="Nevada">NV</option>
+                        <option value="New Hampshire">NH</option>
+                        <option value="New Jersey">NJ</option>
+                        <option value="New Mexico">NM</option>
+                        <option value="New York">NY</option>
+                        <option value="North Carolina">NC</option>
+                        <option value="North Dakota">ND</option>
+                        <option value="Ohio">OH</option>
+                        <option value="Oklahoma">OK</option>
+                        <option value="Oregon">OR</option>
+                        <option value="Pennsylvania">PA</option>
+                        <option value="Rhode Island">RI</option>
+                        <option value="South Carolina">SC</option>
+                        <option value="South Dakota">SD</option>
+                        <option value="Tennesee">TN</option>
+                        <option value="Texas">TX</option>
+                        <option value="Utah">UT</option>
+                        <option value="Vermont">VT</option>
+                        <option value="Virginia">VA</option>
+                        <option value="Washington">WA</option>
+                        <option value="West Virginia">WV</option>
+                        <option value="Wisconsin">WI</option>
+                        <option value="Wyoming">WY</option>
+                    </select>
+                </div>
+                <br></br>
+                <div className="form-group">
+                    <label>Zipcode</label>
+                    <input type="text" minlength="5" maxlength="9" className="form-control" placeholder="Zipcode" 
+                        onChange={e => this.zipcode = e.target.value}/>
+                </div>
+                <br></br>
+                <button className="btn btn-primary btn-block">Save</button>
+            </form>
+        )
+    }
+}
--- frontend/src/components/Profile/profile.js
+++ frontend/src/components/Profile/profile.js
@@ -2,114 +2,147 @@ import React, {Component} from 'react';
 import ReactDOM from "react-dom";
 
 export default class Profile extends Component {
-    handleSubmit = e => {
-        
+    constructor(props) {
+        super(props);
+
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeAdd1 = this.onChangeAdd1.bind(this);
+        this.onChangeAdd2 = this.onChangeAdd2.bind(this);
+        this.onChangeCity = this.onChangeCity.bind(this);
+        this.onChangeState = this.onChangeState.bind(this);
+        this.onChangeZip = this.onChangeZip.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            fullname: '',
+            address1: '',
+            address2: '',
+            city: '',
+            state: [],
+            zipcode: '',
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            users: ['test name'],
+            fullname: 'test name'
+        })
+    }
+
+    onChangeName(e) {
+        this.setState({
+            fullname: e.target.value
+        });
+    }
+
+    onChangeAdd1(e) {
+        this.setState({
+            address1: e.target.value
+        });
+    }
+
+    onChangeAdd2(e) {
+        this.setState({
+            address2: e.target.value
+        });
+    }
+
+    onChangeCity(e) {
+        this.setState({
+            city: e.target.value
+        });
+    }
+
+    onChangeState(e) {
+        this.setState({
+            state: e.target.value
+        });
+    }
+
+    onChangeZip(e) {
+        this.setState({
+            zipcode: e.target.value
+        });
+    }
+
+    onSubmit(e) {
         e.preventDefault();
-        const data = {
-            name: this.name,
-            address1: this.address1,
-            address2: this.address2,
-            city: this.city,
-            state: this.state,
-            zipcode: this.zipcode
-        };
-        
-        console.log(data);
-        
-    };
+
+        const profile = {
+            fullname: this.state.fullname,
+            address1: this.state.address1,
+            address2: this.state.address2,
+            city: this.state.city,
+            state: this.state.state,
+            zipcode: this.state.zipcode            
+        }
+
+        console.log(profile);
+
+        window.location = '/';
+    }
+
     render() {
         return (
-            <form>
+            <div>
                 <h3>Profile</h3>
-
-                <div className="form-group">
-                    <label>Full name</label>
-                    <input type="text" maxlength="50" className="form-control" placeholder="Full name" 
-                        onChange={e => this.name = e.target.value}/>
-                </div>
-                <br></br>
-                <div className="form-group">
-                    <label>Address 1</label>
-                    <input type="text" maxlength="100" className="form-control" placeholder="Address 1" 
-                        onChange={e => this.address1 = e.target.value}/>
-                </div>
-                <br></br>
-                <div className="form-group">
-                    <label>Address 2</label>
-                    <input type="text" maxlength="100" className="form-control" placeholder="Address 2" 
-                        onChange={e => this.address2 = e.target.value}/>
-                </div>
-                <br></br>
-                <div className="form-group">
-                    <label>City</label>
-                    <input type="text" maxlength="100" className="form-control" placeholder="City" 
-                        onChange={e => this.city = e.target.value}/>
-                </div>
-                <br></br>
-                <div className="form-group">
-                    <label>State</label>
-                    <select onChange={e => this.state = e.target.value}>
-                        <option value="Alabama">AL</option>
-                        <option value="Alaska">AK</option>
-                        <option value="Arizona">AZ</option>
-                        <option value="Arkansas">AR</option>
-                        <option value="California">CA</option>
-                        <option value="Colorado">CO</option>
-                        <option value="Conneticut">CT</option>
-                        <option value="Delaware">DE</option>
-                        <option value="Florida">FL</option>
-                        <option value="Georgia">GA</option>
-                        <option value="Hawaii">HI</option>
-                        <option value="Idaho">ID</option>
-                        <option value="Illinois">IL</option>
-                        <option value="Indiana">IN</option>
-                        <option value="Iowa">IA</option>
-                        <option value="Kansas">KS</option>
-                        <option value="Kentucky">KY</option>
-                        <option value="Louisiana">LA</option>
-                        <option value="Maine">ME</option>
-                        <option value="Maryland">MD</option>
-                        <option value="Massachusetts">MA</option>
-                        <option value="Michigan">MI</option>
-                        <option value="Minnesota">MN</option>
-                        <option value="Mississippi">MS</option>
-                        <option value="Missouri">MO</option>
-                        <option value="Montana">MT</option>
-                        <option value="Nebraska">NE</option>
-                        <option value="Nevada">NV</option>
-                        <option value="New Hampshire">NH</option>
-                        <option value="New Jersey">NJ</option>
-                        <option value="New Mexico">NM</option>
-                        <option value="New York">NY</option>
-                        <option value="North Carolina">NC</option>
-                        <option value="North Dakota">ND</option>
-                        <option value="Ohio">OH</option>
-                        <option value="Oklahoma">OK</option>
-                        <option value="Oregon">OR</option>
-                        <option value="Pennsylvania">PA</option>
-                        <option value="Rhode Island">RI</option>
-                        <option value="South Carolina">SC</option>
-                        <option value="South Dakota">SD</option>
-                        <option value="Tennesee">TN</option>
-                        <option value="Texas">TX</option>
-                        <option value="Utah">UT</option>
-                        <option value="Vermont">VT</option>
-                        <option value="Virginia">VA</option>
-                        <option value="Washington">WA</option>
-                        <option value="West Virginia">WV</option>
-                        <option value="Wisconsin">WI</option>
-                        <option value="Wyoming">WY</option>
-                    </select>
-                </div>
-                <br></br>
-                <div className="form-group">
-                    <label>Zipcode</label>
-                    <input type="text" minlength="5" maxlength="9" className="form-control" placeholder="Zipcode" 
-                        onChange={e => this.zipcode = e.target.value}/>
-                </div>
-                <br></br>
-                <button className="btn btn-primary btn-block">Save</button>
-            </form>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Full name</label>
+                        <input type="text" maxlength="50" className="form-control" placeholder="Full name" 
+                            value = {this.state.fullname}
+                            onChange={this.onChangeName}/>
+                    </div>
+                    <br></br>
+                    <div className="form-group">
+                        <label>Address 1</label>
+                        <input type="text" maxlength="100" className="form-control" placeholder="Address 1" 
+                            value = {this.state.address1}
+                            onChange={this.onChangeAdd1}/>
+                    </div>
+                    <br></br>
+                    <div className="form-group">
+                        <label>Address 2</label>
+                        <input type="text" maxlength="100" className="form-control" placeholder="Address 2" 
+                            value = {this.state.address2}
+                            onChange={this.onChangeAdd2}/>
+                    </div>
+                    <br></br>
+                    <div className="form-group">
+                        <label>City</label>
+                        <input type="text" maxlength="100" className="form-control" placeholder="City" 
+                            value = {this.state.city}
+                            onChange={this.onChangeCity}/>
+                    </div>
+                    <br></br>
+                    <div className="form-group">
+                        <label>State</label>
+                        <select classname = "form-control" 
+                            value={this.state.state}
+                            onChange={this.onChangeState}>
+                            {
+                                this.state.state.map(function(state) {
+                                    return <option
+                                    key={state}
+                                    value={state}>{state}
+                                    </option>
+                                })
+                            }                   
+                        </select>
+                    </div>
+                    <br></br>
+                    <div className="form-group">
+                        <label>Zipcode</label>
+                        <input type="text" minlength="5" maxlength="9" className="form-control" placeholder="Zipcode" 
+                            value = {this.state.zipcode}
+                            onChange={this.onChangeZip}/>
+                    </div>
+                    <br></br>
+                    <button className="btn btn-primary btn-block">Save</button>
+                </form>
+            </div>
         )
     }
 }
